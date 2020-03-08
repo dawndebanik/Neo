117e210d01c2d44edfb3581b12db95c7ff7feb9c
Debanik <debanik.dawn@thoughtworks.com>
2020-03-08 11:19:51 +0530

    Initial commit

---
+++ src/main/kotlin/com/hobbies/neo/NeoApplication.kt
@@ -0,0 +1,11 @@
+package com.hobbies.neo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NeoApplication
+
+fun main(args: Array<String>) {
+	runApplication<NeoApplication>(*args)
+}
